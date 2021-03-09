19eb5b837b252587193e95572e118cb59896374f
romuald85 <papeboyedk@gmail.com>
2021-03-09 19:11:30 +0100

    Feature first section

--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="bg-light">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row bg-light">
-                <div class="pt-5 col-md-6">
+                <div class="p-5 col-md-6">
                     <h1 class="text-center">بسم الله الرحمن الرحيم</h1>
                     <p class="mt-3 text-center">L'institut Ibn Maalik vous aide à devenir arabophone en suivant des cours de langue arabe en ligne</p>
                     <p class="mt-3 text-center">Apprendre l'arabe en ligne est idéal pour ceux qui n'ont pas la possibilité de voyager.Le programme de l'institut vous permettra d'avancer session par session jusqu'à atteindre un niveau avancé pour la compréhension de la langue ainsi qu'une maîtrise de ses règles de grammaire.Il sera aussi introduit dans le programme des sessions dédiées à l'oral pour maîtriser le parler.</p>
@@ -10,9 +10,8 @@
                     <p class="mt-3 text-center">Nos cours sont ouverts à toutes et à tous sans limite d'âge (hommes, femmes et enfants).</p>
                     <p class="text-start text-center">L'inscription est simple remplissez notre formulaire:<span><a href="#"> cliquez ici</a></span></p>
                 </div>
-                <div class="col-md-6">
-                    <div class="bg-color">
-                    </div>
+                <div class="bg-color-first col-md-6 nopadding">
+                    <img class="macbook" src="assets/images/macbook.jpg" alt="">
                 </div>
             </div>
         </div>
@@ -80,23 +79,19 @@ export default {
 }
 </script>
 <style scoped>
-    .bg-color{
-        background-color: #8CD9F1;
-        width: 100%;
-        height: 100%;
-        transform: translateX(45%);
+    .bg-color-first{
+        background: linear-gradient(to right, #f8f9fa 30%, #8CD9F1 0%);
+        display: flex;
+        align-items: center;
     }
 
-    .bg-color:before{
-        background: url('/assets/images/macbook.jpg') center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        display: inline-block;
-        width: 100%; 
-        height: 85%;
-        content:"";
-        transform: translateX(-40%);
-        margin-top: 11%;
-        box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
+    .nopadding {
+        padding: 0 !important;
+        margin: 0 !important;
+    }
+
+    .macbook{
+        width: 90%;
+        height: auto;
     }
 </style>
