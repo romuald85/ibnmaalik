e7a851b9596bcf0cc5f39519d6d318ec3fa1cc3b
romuald85 <papeboyedk@gmail.com>
2021-07-04 20:33:30 +0200

    Add aos library

--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -8,7 +8,7 @@
                     <p class="mt-3 text-center">Apprendre l'arabe en ligne est idéal pour ceux qui n'ont pas la possibilité de voyager.Le programme de l'institut vous permettra d'avancer session par session jusqu'à atteindre un niveau avancé pour la compréhension de la langue ainsi qu'une maîtrise de ses règles de grammaire.Il sera aussi introduit dans le programme des sessions dédiées à l'oral pour maîtriser le parler.</p>
                     <p class="mt-3 text-center">Nous vous proposons un programme structuré avec des enseignants expérimentés d'Égypte habitués à enseigner aux élèves non arabophones.</p>
                     <p class="mt-3 text-center">Nos cours sont ouverts à toutes et à tous sans limite d'âge (hommes, femmes et enfants).</p>
-                    <p class="text-start text-center">L'inscription est simple remplissez notre formulaire:<span><a href="#"> cliquez ici</a></span></p>
+                    <p class="text-start text-center">L'inscription est simple remplissez notre formulaire:<span><a href="register"> cliquez ici</a></span></p>
                 </div>
                 <div class="bg-color-first col-md-6 nopadding">
                     <img class="macbook" src="assets/images/macbook.jpg" alt="laptop">
